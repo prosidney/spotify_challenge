cbdc7696573f70d92b25a1ae07cea8649c505480
Sidney <sidney.assis@nike.com>
2019-12-18 15:05:46 +0100

    refactored

--- src/main/java/diagonaldifference/Result.java
+++ src/main/java/diagonaldifference/Result.java
@@ -11,26 +11,14 @@ public class Result {
      */
 
     public static int diagonalDifference(List<List<Integer>> arr) {
-        int primaryDiagonal = getPrimaryDiagonal(arr);
-
-        int secondaryDiagonal = getSecondaryDiagonal(arr);
+        int primaryDiagonal = 0;
+        int secondaryDiagonal = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            primaryDiagonal += arr.get(i).get(i);
+            secondaryDiagonal += arr.get(i).get((arr.size() -1) - i);
+        }
 
         return (primaryDiagonal - secondaryDiagonal) * -1;
     }
 
-    private static int getSecondaryDiagonal(List<List<Integer>> arr) {
-        int result = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            result += arr.get(i).get((arr.size() -1) - i);
-        }
-        return result;
-    }
-
-    private static int getPrimaryDiagonal(List<List<Integer>> arr) {
-        int result = 0;
-        for (int i = 0; i < arr.size(); i++) {
-            result += arr.get(i).get(i);
-        }
-        return result;
-    }
 }
